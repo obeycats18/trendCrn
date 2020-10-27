9b9e401649b675cbe58190b741c6664ae76ba8d8
Dima Kapish <dima.development@fakel.com.ua>
2020-10-27 23:41:49 +0200

    Updates

---
+++ precache-manifest.54d377859e85fe8a40199450982a761d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d0a0e6d1ff345ff7b375ffbddc4526d",
+    "url": "/trendCrn/index.html"
+  },
+  {
+    "revision": "8318e9c93aafb8043389",
+    "url": "/trendCrn/static/css/2.f8e83cc6.chunk.css"
+  },
+  {
+    "revision": "19e40a7a26b7ebc3912e",
+    "url": "/trendCrn/static/css/main.293a8b86.chunk.css"
+  },
+  {
+    "revision": "8318e9c93aafb8043389",
+    "url": "/trendCrn/static/js/2.527c8cc7.chunk.js"
+  },
+  {
+    "revision": "c4a592068352b63b56b164cbe61e876b",
+    "url": "/trendCrn/static/js/2.527c8cc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19e40a7a26b7ebc3912e",
+    "url": "/trendCrn/static/js/main.4ea74f63.chunk.js"
+  },
+  {
+    "revision": "d2604d95932a8ca53a53",
+    "url": "/trendCrn/static/js/runtime-main.097aee9d.js"
+  },
+  {
+    "revision": "47bf9c599a99bd55e5277398ae4ca49b",
+    "url": "/trendCrn/static/media/logo.47bf9c59.png"
+  }
+]);
